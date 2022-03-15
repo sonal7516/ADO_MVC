11a306faf217a11702277aebcc22fa7c05520ba4
Sonal Brajraj Singh <99856906+sonal7516@users.noreply.github.com>
2022-03-15 18:09:42 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SonalCRUD_ADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SonalCRUD_ADO.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        StudentDataAccess dataAccess;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            dataAccess = new StudentDataAccess();
+        }
+
+        public IActionResult Index()
+        {
+            var std = dataAccess.GetStudents();
+            return View(std);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
